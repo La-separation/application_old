8d650c28d44a928b493f1671f7fa253d9a72a445
N3mesis94 <adrienjacquet98@gmail.com>
2013-11-19 14:45:50 +0100

    modification de l'import des sons

--- scripts/libs/separation_toolkit/sound/polices.js
+++ scripts/libs/separation_toolkit/sound/polices.js
@@ -1,3 +1,9 @@
-
+/*
+ * cut --> coupable
+ * rub1, rub2 --> ombre
+ * tear, tear1, tear2 --> centrale
+ * ambiant
+ * tap
+*/
 
 scriptLoaded('scripts/libs/separation_toolkit/sound/polices.js');
--- scripts/libs/separation_toolkit/sound/sound_play.js
+++ scripts/libs/separation_toolkit/sound/sound_play.js
@@ -22,14 +22,14 @@ function initSounds() {
 		var path = location.pathname;
 		var tab = path.split("/")
 		sounds["cut"] = new Media(path.replace(tab[tab.length-1], "sounds/cut.ogg"));
-		sounds["rub"] = new Media(path.replace(tab[tab.length-1], "sounds/rub.ogg"));
+		sounds["rub1"] = new Media(path.replace(tab[tab.length-1], "sounds/rub1.ogg"));
+		sounds["rub2"] = new Media(path.replace(tab[tab.length-1], "sounds/rub2.ogg"));
 		sounds["tear"] = new Media(path.replace(tab[tab.length-1], "sounds/tear.ogg"));
+		sounds["tear1"] = new Media(path.replace(tab[tab.length-1], "sounds/tear1.ogg"));
+		sounds["tear2"] = new Media(path.replace(tab[tab.length-1], "sounds/tear2.ogg"));
 		sounds["ambiant"] = new Media(path.replace(tab[tab.length-1], "sounds/ambiant.ogg"));
-		sounds["error"] = new Media(path.replace(tab[tab.length-1], "sounds/error.ogg"));
+//		sounds["error"] = new Media(path.replace(tab[tab.length-1], "sounds/error.ogg"));
 		sounds["tap"] = new Media(path.replace(tab[tab.length-1], "sounds/tap.ogg"));
-		sounds["cut1"] = new Media(path.replace(tab[tab.length-1], "sounds/cut1.ogg"));
-		sounds["cut2"] = new Media(path.replace(tab[tab.length-1], "sounds/cut2.ogg"));
-		sounds["login"] = new Media(path.replace(tab[tab.length-1], "sounds/login.ogg"));
 	}
 }
 
