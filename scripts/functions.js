4dcbee1a1fbcaa10e380c8933516ef4db6a0646c
N3mesis94 <adrienjacquet98@gmail.com>
2013-11-19 14:21:08 +0100

    ajout de la fonction getWordCodes

--- scripts/functions.js
+++ scripts/functions.js
@@ -24,4 +24,26 @@ function randArray(array) {
 	return array;
 }
 
+function getWordCodes(word, police) {
+	var word_codes = [""];
+	var i=0;
+	while (i < word.length) {
+		var ilist = code[police][word[i].charCodeAt(0) - "a".charCodeAt(0)];
+		var j=1;
+		var k=0;
+		while (j <= ilist.length) {
+			if (j != 1) {
+				word_codes=word_codes.concat(word_codes);
+			}
+			while (k < (word_codes.length*j/ilist.length)) {
+				word_codes[k]=word_codes[k] + ilist[j-1];
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	return word_codes;
+}
+
 scriptLoaded('scripts/functions.js');
