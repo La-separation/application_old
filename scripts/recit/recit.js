0946db56fd77686542de3b717a27bb8c4e2d3764
N3mesis94 <adrienjacquet98@gmail.com>
2013-11-21 19:00:19 +0100

    menu recits aléatoire non generique

--- scripts/recit/recit.js
+++ scripts/recit/recit.js
@@ -34,23 +34,43 @@ Recit.displayStoriesMenu = function() {
 	Destroy.all();
 	Gui.Recit.menuDisplayAll();
 	
-	nb_recit_max = Recit.cst.line.nb;
+	//~ nb_recit_max = Recit.cst.line.nb;
+//~ 
+	//~ var path= "";
+	//~ 
+	//~ for (var i=nb_recit_max*(story_page-1); i<xmlList.length && i<=(nb_recit_max*story_page); i++) {
+		//~ path="stories/"+xmlList[i]+".xml";
+		//~ var xml_file = Xml.load(path);
+//~ 
+		//~ titles[i] = new Word(xml_file.getElementsByTagName("title")[0].textContent, null, 0);
+	    //~ titles[i].setCenterX(screenWidth/2);
+	    //~ titles[i].setCenterY((((screenHeight)/(nb_recit_max+1))*(i-nb_recit_max*(story_page-1)+0.5)));
+	    //~ titles[i].display(mainLayer);
+	    //~ titles[i].onTap(function(path){
+	        //~ return function(){Recit.openStory(path);}
+		//~ }(path));
+	//~ }
 
-	var path= "";
+	var w = screenWidth;
+	var h = screenHeight;
+
+	var mots=[];
+	mots[0]=[new Word("Separation"),2,w*0.5,h*0.4]; 
+	mots[1]=[new Word("Paysage"),1.3,w*0.6,h*0.15]; 
+	mots[2]=[new Word("Antarfrique"),1.2,w*0.7,h*0.65];
+	mots[3]=[new Word("Amant"),1.4,w*0.8,h*0.8]; 
+	mots[4]=[new Word("Decision"),1.2,w*0.3,h*0.2];
+	mots[5]=[new Word("Horloge"),1.5,w*0.1,h*0.5];
+	mots[6]=[new Word("Derisoire"),1.9,w*0.3,h*0.85]; 
+	mots[7]=[new Word("Epluchage"),1.4,w*0.9,h*0.25]; 
+	mots[8]=[new Word("Infini"),1.6,w*0.3,h*0.6];
 	
-	for (var i=nb_recit_max*(story_page-1); i<xmlList.length && i<=(nb_recit_max*story_page); i++) {
-		path="stories/"+xmlList[i]+".xml";
-		var xml_file = Xml.load(path);
-
-		titles[i] = new Word(xml_file.getElementsByTagName("title")[0].textContent, null, 0);
-	    titles[i].setCenterX(screenWidth/2);
-	    titles[i].setCenterY((((screenHeight)/(nb_recit_max+1))*(i-nb_recit_max*(story_page-1)+0.5)));
-	    titles[i].display(mainLayer);
-	    titles[i].onTap(function(path){
-	        return function(){Recit.openStory(path);}
-		}(path));
+	for (var i in mots) {
+		mots[i][0].setCenterXY(mots[i][2],mots[i][3]);
+		mots[i][0].setZoom(mots[i][1]-0.5);
+		mots[i][0].display(mainLayer)
 	}
-
+	mots[0][0].onTap(function(){Recit.openStory("stories/separation.xml")});
 	mainLayer.draw();
 	actionLayer.draw();
 }
